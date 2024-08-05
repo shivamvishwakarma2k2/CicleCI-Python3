cba7c506938840ebbf576e51cb6f1733a88fd35e
shivamvishwakarma2k2 <shivamvishwakarma2k2@gmail.com>
2024-08-05 09:50:09 +0530

    update

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import to_upper
+
+class MyTestCase(unittest.TestCase):
+    def test_to_upper(self):
+        name = 'Shivam'
+        upper_name = to_upper(name)
+        self.assertEqual(upper_name, "SHIVAM")
+        
+if __name__ == '__main__':
+    unittest.main()
